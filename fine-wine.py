b27bf6303beda94badeadfaf6c3f08c6c59f17a1
Julian Janisch <julianjanisch23@gmail.com>
2020-08-06 18:01:46 +0200

    Get random words and print wine description

--- fine-wine.py
+++ fine-wine.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+import random
+
+def getRandIndex(word_array):
+    return random.randint(0, len(word_array))
+
 def main():
 
     with open("adjectives.txt", "r") as f:
@@ -10,5 +15,11 @@ def main():
         nouns = f.readlines()
         nouns = [line.strip() for line in nouns]
 
+    adjective_1 = adjectives[getRandIndex(adjectives)]
+    adjective_2 = adjectives[getRandIndex(adjectives)]
+    noun_1 = nouns[getRandIndex(nouns)]
+
+    print(f"A {adjective_1} wine with hints of {adjective_2} {noun_1}.")
+    
 if __name__ == "__main__":
     main()
