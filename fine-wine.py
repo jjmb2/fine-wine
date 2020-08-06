2d0f1cbc73d3911680fac8b68253e71a1780af0c
Julian Janisch <julianjanisch23@gmail.com>
2020-08-06 18:12:24 +0200

    Add function to read words into an array

--- fine-wine.py
+++ fine-wine.py
@@ -2,18 +2,19 @@
 
 import random
 
+def txt_to_list(file_name):
+    with open(file_name, "r") as f:
+        word_list = f.readlines()
+        word_list = [line.strip() for line in word_list]
+    return word_list
+
 def get_rand_index(word_array):
     return random.randint(0, len(word_array))
 
 def main():
 
-    with open("adjectives.txt", "r") as f:
-        adjectives = f.readlines()
-        adjectives = [line.strip() for line in adjectives]
-
-    with open("nouns.txt", "r") as f:
-        nouns = f.readlines()
-        nouns = [line.strip() for line in nouns]
+    adjectives = txt_to_list("adjectives.txt")
+    nouns = txt_to_list("nouns.txt")
 
     adjective_1 = adjectives[get_rand_index(adjectives)]
     adjective_2 = adjectives[get_rand_index(adjectives)]
